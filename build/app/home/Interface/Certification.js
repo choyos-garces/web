7bf9af43f8b8a45d2d9488c88eb86761609938fb
Christopher Hoyos <choyos.garces@gmail.com>
2016-03-05 17:41:47 -0500

    add svg, change layout

---
+++ build/app/home/Interface/Certification.js
@@ -0,0 +1,9 @@
+var Certification = (function () {
+    function Certification(n, l, u) {
+        this.name = n;
+        this.logo = l;
+        this.url = u;
+    }
+    return Certification;
+})();
+exports.Certification = Certification;
---
+++ build/app/home/Interface/Partner.js
@@ -0,0 +1,9 @@
+var Partner = (function () {
+    function Partner(l, u, n) {
+        this.name = n;
+        this.logo = l;
+        this.url = u;
+    }
+    return Partner;
+})();
+exports.Partner = Partner;
